dd6902e2cf3a920dac572279c449395566116db3
trinhtan <lop12a1trinhvantan@gmail.com>
2018-09-06 11:35:49 +0700

    Finish Static pages
    Finish chapter 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world"
   end
+
+  before_action :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
