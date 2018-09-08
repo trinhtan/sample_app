7cbee337b1a32369abab9cc921bff7693fdcecee
trinhtan <lop12a1trinhvantan@gmail.com>
2018-09-08 15:28:45 +0700

    Chapter 6: Modeling users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about", as: :about
   get "/contact", to: "static_pages#contact", as: :contact
   get "/signup", to: "users#new"
+  get "/signup", to: 'users#create'
+  resources :users
   scope "(:locale)", locale: /en|vi/ do
     root to: "static_pages#home"
   end
